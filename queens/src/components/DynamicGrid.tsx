c909452aec14a86afefb403cc03f7ff9782923f0
Kayvia Harriott <kayviaharriott@hotmail.com>
2024-11-15 23:18:49 -0500

    Added state logic for boxes when clicked

--- queens/src/components/DynamicGrid.tsx
+++ queens/src/components/DynamicGrid.tsx
@@ -1,29 +1,33 @@
 import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
-import { QueenIcon } from "./icons/queen_icon";
-import { CrossIcon } from "./icons/cross_icon";
-import CrossIcon_SVG from "../components/icons/cross_icon.svg"
-import QueenIcon_SVG from "../components/icons/queen_icon.svg"
+import CrossIcon_SVG from "../components/icons/cross_icon.svg";
+import QueenIcon_SVG from "../components/icons/queen_icon.svg";
+
 const DynamicGrid = () => {
+  const states = ["none", "cross", "queen"];
+  const [box, setBox] = useState(states[0]);
+
+  const toggleBoxState = () => {
+    setBox((prev) => states[(states.indexOf(prev) + 1) % states.length]);
+  };
+
   return (
     <div>
-      
-       {[<img src={QueenIcon_SVG} alt="Cross Icon" />, <img src={CrossIcon_SVG} alt="Cross Icon" />].map((item, index) => (
-        <Box
-        key={index}
+      <Box
+        onClick={toggleBoxState}
         sx={{
           border: "1px solid black",
           width: "50px",
-          height: "50px", // Ensures box has a fixed size
-          padding: "8px",
-          display: "flex", // Enables flexbox
-          justifyContent: "center", // Centers horizontally
-          alignItems: "center", // Centers vertically
+          height: "50px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        {item}
+        {box === "none" && <p></p>}
+        {box === "cross" && <img src={CrossIcon_SVG} alt={"Cross"} />}
+        {box === "queen" && <img src={QueenIcon_SVG} alt={"Queen"} />}
       </Box>
-      ))}
     </div>
   );
 };
